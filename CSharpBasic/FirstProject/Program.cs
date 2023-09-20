b32c6d21dada62a4d653a5efb1b9b49a277d3ad9
YoungMin Cho (Luke) <83196845+luke-youngmin-cho@users.noreply.github.com>
2023-09-20 17:50:40 +0900

    CSharp - Reviewing
    - Reviewed basics of C# & OOP.
    - Concept of render pipeline.

---
+++ CSharpBasic/FirstProject/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace FirstProject
+{
+    public class Human
+    {
+        public static Human instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new Human();
+                return _instance;
+            }
+        }
+        private static Human _instance;
+
+        public string name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        string _name;
+        float _height;
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public void SetName(string value)
+        {
+            _name = value;
+        }
+
+        public Human()
+        {
+
+        }
+
+        ~Human() { }
+
+        public void SayHi()
+        {
+            Console.WriteLine("Hi");
+        }
+    }
+
+    public class Luke : Human
+    {
+        public float weight;
+
+        public void SayHi()
+        {
+            Console.WriteLine("Hi, I'm Luke");
+        }
+    }
+
+    public class A { }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human human = new Human();
+
+            Luke luke = new Luke();
+            human = luke;
+            human.SayHi();
+
+            Human human2 = new Luke();
+        }
+    }
+}
